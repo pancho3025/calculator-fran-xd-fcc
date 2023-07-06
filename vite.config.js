038201901f5c35e9ea5d04b5f66a26463ea80563
francisco espeche <99774117+pancho3025@users.noreply.github.com>
2023-07-05 21:58:16 -0300

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gitlab.com/pancho3025/calculator-fcc-fran/'
+  base: 'https://gitlab.com/pancho3025/calculator-fran-xd-fcc/'
 })
